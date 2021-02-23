4c42cc3af2742e8f74c59e42727cbbe565ee56b4
Ding Guangyu <guangyu.ding@okcoin.com>
2021-02-23 16:16:12 +0800

    client

--- aeron-samples/src/main/java/io/aeron/samples/cluster/tutorial/BasicAuctionClusterClient.java
+++ aeron-samples/src/main/java/io/aeron/samples/cluster/tutorial/BasicAuctionClusterClient.java
@@ -209,6 +209,7 @@ public class BasicAuctionClusterClient implements EgressListener
         try (
             MediaDriver mediaDriver = MediaDriver.launchEmbedded(new MediaDriver.Context()                      // <1>
                 .threadingMode(ThreadingMode.SHARED)
+                    .conductorIdleStrategy(new BusySpinIdleStrategy())
                 .dirDeleteOnStart(true)
                 .dirDeleteOnShutdown(true));
             AeronCluster aeronCluster = AeronCluster.connect(
